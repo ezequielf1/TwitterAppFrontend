42cb511bc1ea714e4f9c368cf347da4251c403a8
Brian Ezequiel Fritz <brian.fritz@Macbook-205.local>
2020-07-06 11:41:20 -0300

    Initial commit

---
+++ TwitterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterApp
+//
+//  Created by Brian Ezequiel Fritz on 06/07/2020.
+//  Copyright © 2020 Brian Ezequiel Fritz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
